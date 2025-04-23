af653769b7552a5ccc60b4cbfe26010dbfbfc352
Samuel Silva <54758700+Samoody@users.noreply.github.com>
2025-04-23 14:23:27 -0300

    Create IdadeUsuario.java

---
+++ IdadeUsuario.java
@@ -0,0 +1,19 @@
+/*
+ * RA: 12123150
+ * AUTOR: SAMUEL KLEBER VIEIRA SILVA
+ * DAT
+ * Descrição: Programa que solicita a idade do usuário e exibe o valor informado.
+ */
+
+import java.util.Scanner;
+
+public class IdadeUsuario {
+    public static void main(String[] args) {
+        // Criando o objeto Scanner para entrada de dados
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando a idade do usuário
+        System.out.print("Por favor, digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        // Exibindo a
